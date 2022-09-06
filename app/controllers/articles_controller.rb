c7827a1a544d65cdcda9bb41a2fee8bff1abd7fd
Muhammad Waqas <engr.waqas1030@gmail.com>
2022-09-06 15:15:14 +0500

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def show
+    # @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article =Article.new
+  end
+
+  def edit
+    # @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    # @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  root "pages#home"
+  get 'about', to: 'pages#about'
+end
