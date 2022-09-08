58da7e0f4ea30a0fed3806688876554b5a547f66
Muhammad Waqas <engr.waqas1030@gmail.com>
2022-09-08 16:13:42 +0500

    association created one to many

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :body, presence: true, length: {minimum: 6, maximum: 500}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_094054) do
+ActiveRecord::Schema.define(version: 2022_09_08_104232) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
