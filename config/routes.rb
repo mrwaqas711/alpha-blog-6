fed74e0e0e0a9d1149d51e8518ef2ebd41f85019
Muhammad Waqas <engr.waqas1030@gmail.com>
2022-09-08 19:38:39 +0500

    sign up user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   root "pages#home"
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
