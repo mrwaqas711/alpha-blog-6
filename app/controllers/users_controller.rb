d1c4537b199aa6cfe34bab8841dd05254135acd0
Muhammad Waqas <engr.waqas1030@gmail.com>
2022-09-09 21:58:03 +0500

    add user articles and user profile show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -26,8 +32,6 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
-
 
   private
   def user_params
