745ae4043be3f30ee50ce430a14537b168e32807
Muhammad Waqas <engr.waqas1030@gmail.com>
2022-09-13 21:08:17 +0500

    added test and integration tests and categories validations

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,43 @@
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  public def require_admin
+    if !( logged_in? && current_user.admin?)
+      flash[:alert] = "Only admin can perform that action"
+      redirect_to categories_path
+    end
+  end
+
+end
